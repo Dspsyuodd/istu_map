c5a44b2143e6430635906f676eb566edb0e1ec78
Dspsyuodd <varlamovartem1111@gmail.com>
2024-06-25 21:58:58 +0400

    new permissions

--- lib/features/map/external_map/data/repositories/geoposition_repository_impl.dart
+++ lib/features/map/external_map/data/repositories/geoposition_repository_impl.dart
@@ -1,16 +1,15 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:fpdart/fpdart.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
 import '../../../../../core/errors/failure.dart';
 import '../../domain/repositories/geoposition_repository.dart';
 
 class GeopositionRepositoryImpl implements GeopositionRepository {
   GeopositionRepositoryImpl();
-  final List<StreamSubscription<Position>> _subscribtions = [];
+  final List<StreamSubscription<LocationData>> _subscribtions = [];
 
   @override
   void dispose() {
@@ -21,9 +20,8 @@ class GeopositionRepositoryImpl implements GeopositionRepository {
 
   @override
   void addListener(void Function(Either<Failure, LatLng>) listener) {
-    var subscription = Geolocator.getPositionStream().listen((position) {
-      var latlng = LatLng(position.latitude, position.longitude);
-      print(latlng.toString());
+    var subscription = Location().onLocationChanged.listen((position) {
+      var latlng = LatLng(position.latitude ?? 0, position.longitude ?? 0);
       listener(Right(latlng));
     });
 
@@ -32,8 +30,7 @@ class GeopositionRepositoryImpl implements GeopositionRepository {
 
   @override
   Future<Either<Failure, LatLng>> getCurrentPosition() async {
-    var position = await Geolocator.getCurrentPosition();
-    print(position.toString());
-    return Right(LatLng(position.latitude, position.longitude));
+    var position = await Location().getLocation();
+    return Right(LatLng(position.latitude ?? 0, position.longitude ?? 0));
   }
 }
